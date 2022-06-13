5927b9439a046adb1da16d5a0b7bb31cc3e9bbf7
tghgrs <ucyukun@live.com>
2022-06-14 00:36:28 +0800

    配置视图解析器

--- src/main/java/com/fic091/controller/MyFirstController.java
+++ src/main/java/com/fic091/controller/MyFirstController.java
@@ -1,8 +1,13 @@
 package com.fic091.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyFirstController {
-
+    @RequestMapping("/hello")
+    public String myFirstRequest(){
+        System.out.println("请求收到了,正在处理中");
+        return "hello";
+    }
 }
